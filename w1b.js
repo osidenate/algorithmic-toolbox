378edac28b7e9ff2731e6cbf5de2a3b12167cfe5
Nathan Nelson <osidenate@gmail.com>
2016-07-01 21:20:19 -0700

    maximum pairwise product fast

--- w1b.js
+++ w1b.js
@@ -23,11 +23,40 @@ const maxPairwiseProduct = (numbers) => {
     return max;
 };
 
+const maxPairwiseProductFast = (numbers) => {
+    const length = numbers.length;
+    const cache = [];
+    let max = 0;
+
+    for (let i = 0; i < length; i++) {
+        cache[i] = [];
+
+        for (let g = 0; g < length; g++) {
+            if (i === g) continue;
+
+            let product;
+
+            if (cache[g] && cache[g][i]) {
+                product = cache[g][i];
+            } else {
+                product = cache[i][g] = numbers[i] * numbers[g]
+            }
+
+            if (product > max) {
+                max = product;
+            }
+        }
+    }
+
+    return max;
+};
+
+
 rl.question('Input: ', (line) => {
     const words = line.split(' ');
     const numbers = words.map(word => parseInt(word, 10));
 
-    console.log(maxPairwiseProduct(numbers));
+    console.log(maxPairwiseProductFast(numbers));
 
     rl.close();
 });
